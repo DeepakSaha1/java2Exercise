68ef252c4d229073e18955aef215b2fae929c919
DeepakSaha1 <deepak.saha@tothenew.com>
2019-02-26 15:25:51 +0530

    String Sorting

---
+++ ques2.java
@@ -0,0 +1,27 @@
+// 2. WAP to sorting string without using string Methods?.
+import java.util.Scanner;
+
+class StringSort {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string to sort: ");
+        char[] s = sc.nextLine().toCharArray();
+
+        //Bubble sort
+        int n = s.length;
+        for (int i = 0; i < n-1; i++){
+            for (int j = 0; j < n-i-1; j++) {
+                if (s[j] > s[j + 1]) {
+                    char temp = s[j];
+                    s[j] = s[j + 1];
+                    s[j + 1] = temp;
+                }
+            }
+        }
+        System.out.print("Sorted string is ");
+        System.out.println(s);
+
+    }
+}
